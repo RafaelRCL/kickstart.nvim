37d4fdf170cd2079719018ea19daa25d36584af8
Rafael Campos de Lima <rafaelrcamposl@gmail.com>
2023-02-23 11:35:07 -0300

    Add papercolor theme; Add laguages ensure install for tree-sitter

--- init.lua
+++ init.lua
@@ -287,7 +287,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim', 'javascript', 'html', 'vue', 'scss' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -407,11 +407,11 @@ end
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  -- tsserver = {},
-
+  tsserver = {},
+  volar = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
